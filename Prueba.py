f0b95dba462a6cfbd15721e1f74d5549b1fb885b
HaresNeme <haresneme@gmail.com>
2020-05-25 12:39:42 -0500

    My Commit

---
+++ Prueba.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  1 20:49:58 2019
+
+@author: Hares Neme
+"""
+!git init
+'''!git add README.md'''
+!git add "Prueba.py"
+!git commit -m "My Commit"
+!git config user.email haresneme@gmail.com
+!git config user.name HaresNeme
+'''
+!git remote add origin https://github.com/HaresNeme/Test-.git
+!git push origin master'''
+
+print ('hello world')
+print ('hellow anaconda')
