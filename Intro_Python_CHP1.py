188aecd39524eb88736898597675336bb8e66dbb
HaresNeme <haresneme@gmail.com>
2020-05-31 20:53:37 -0500

    initializing repository

---
+++ Intro_Python_CHP1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 31 16:52:23 2020
+
+@author: Hares Neme
+"""
+import sklearn 
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+dataset = load_iris()
+
+
--- Prueba.py
+++ Prueba.py
@@ -4,15 +4,8 @@ Created on Sun Sep  1 20:49:58 2019
 
 @author: Hares Neme
 """
-!git init
-'''!git add README.md'''
-!git add "Prueba.py"
-!git commit -m "My Commit"
-!git config user.email haresneme@gmail.com
-!git config user.name HaresNeme
-!git remote add origin https://github.com/HaresNeme/Test-.git
-!git push -u origin master
 
 print ('hello world')
 print ('hellow anaconda')
+
 
